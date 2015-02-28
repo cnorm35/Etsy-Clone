e607628d0ba0fb365e037320c16ab15069ef3c70
Cody <wcodynorman@gmail.com>
2015-02-28 17:40:37 -0500

    only upload to dropbox on production/heroku

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,13 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :image, styles: { medium: "200x", thumb: "100x100"}, default_url: "default.jpg",
-                                     storage: :dropbox,
-                                     dropbox_credentials: Rails.root.join("config/dropbox.yml")
-                                     
+  if Rails.env.development?
+    has_attached_file :image, styles: { medium: "200x", thumb: "100x100"}, default_url: "default.jpg"
+  else
+    has_attached_file :image, styles: { medium: "200x", thumb: "100x100x"}, default_url: "default.jpg",
+                                        storage: :dropbox,
+                                        dropbox_credentials: Rails.root.join("config/dropbox.yml"),
+                                        path: ":style/:id_:filename"
+  end
+
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-  
+
 end

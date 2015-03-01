4ab8bef180a17774b775d12a28e3e75224b37b1f
Cody <wcodynorman@gmail.com>
2015-03-01 13:08:17 -0500

    removed extra routes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,21 +4,10 @@ class OrdersController < ApplicationController
 
   respond_to :html
 
-  def index
-    @orders = Order.all
-    respond_with(@orders)
-  end
-
-  def show
-    respond_with(@order)
-  end
 
   def new
     @order = Order.new
     @listing = Listing.find(params[:listing_id])
-  end
-
-  def edit
   end
 
   def create
@@ -37,14 +26,12 @@ class OrdersController < ApplicationController
     end
   end
 
-  def update
-    @order.update(order_params)
-    respond_with(@order)
+  def sales
+    @orders = Order.where(seller: current_user).order("created_at DESC")
   end
 
-  def destroy
-    @order.destroy
-    respond_with(@order)
+  def purchases
+    @orders = Order.where(buyer: current_user).order("created_at DESC")
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
   root 'listings#index'
   
   resources :listings do
-    resources :orders
+    resources :orders, only: [:create, :new]
   end
 
   get 'pages/about'
   get 'pages/contact'
-  get 'seller' => "listings#seller"
+  get 'seller' => 'listings#seller'
+  get 'sales' => "orders#sales"
+  get 'purchases' => "orders#purchases"
 
 
   # The priority is based upon order of creation: first created -> highest priority.

7bdbb36c9b6fa1f84471480d16cf70e6e1baab6d
Cody <wcodynorman@gmail.com>
2015-03-01 15:51:41 -0500

    set up transfer to seller accounts

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -4,32 +4,38 @@ class ListingsController < ApplicationController
   before_filter :check_user, only: [:edit, :update, :destroy]
 
 
-  # GET /listings
-  # GET /listings.json
   def index
     @listings = Listing.all.order("created_at DESC")
   end
 
-  # GET /listings/1
-  # GET /listings/1.json
   def show
   end
 
-  # GET /listings/new
   def new
     @listing = Listing.new
   end
 
-  # GET /listings/1/edit
   def edit
   end
 
-  # POST /listings
-  # POST /listings.json
   def create
     @listing = Listing.new(listing_params)
     @listing.user_id = current_user.id
 
+    if current_user.recipient.blank?
+      Stripe.api_key = ENV["STRIPE_API_KEY"]
+      token= params[:stripeToken]
+
+      recipient = Stripe::Recipient.create(
+        name: current_user.name,
+        type: "individual",
+        bank_account: token
+        )
+
+      current_user.recipient = recipient.id
+      current_user.save
+    end
+    
     respond_to do |format|
       if @listing.save
         format.html { redirect_to @listing, notice: 'Listing was successfully created.' }
@@ -41,8 +47,6 @@ class ListingsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /listings/1
-  # PATCH/PUT /listings/1.json
   def update
     respond_to do |format|
       if @listing.update(listing_params)
@@ -55,8 +59,6 @@ class ListingsController < ApplicationController
     end
   end
 
-  # DELETE /listings/1
-  # DELETE /listings/1.json
   def destroy
     @listing.destroy
     respond_to do |format|
@@ -65,17 +67,15 @@ class ListingsController < ApplicationController
     end
   end
 
-  def seller 
+  def seller
     @listings = Listing.where(user: current_user).order("created_at DESC")
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_listing
       @listing = Listing.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def listing_params
       params.require(:listing).permit(:name, :description, :price, :image)
     end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -33,6 +33,12 @@ class OrdersController < ApplicationController
       flash[:danger] = e.message
     end
 
+    transfer = Stripe::Transfer.create(
+      amount: (@listing.price * 95).floor,
+      currency: "usd",
+      recipient: @seller.recipient
+      )
+
     if @order.save
       redirect_to root_url, notice: "Order successfully created!"
     else

242bda794d5e58d28c0c0e107dee71d9d6011227
Cody <wcodynorman@gmail.com>
2015-03-01 10:18:49 -0500

    updated devise views

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'bootstrap-sass'
 gem 'paperclip'
 gem 'paperclip-dropbox'
 gem 'figaro'
+gem 'devise'
 
 
 
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,13 +1,12 @@
 class Listing < ActiveRecord::Base
   if Rails.env.development?
-    has_attached_file :image, styles: { medium: "200x", thumb: "100x100"}, default_url: "default.jpg"
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
   else
-    has_attached_file :image, styles: { medium: "200x", thumb: "100x100x"}, default_url: "default.jpg",
-                                        storage: :dropbox,
-                                        dropbox_credentials: Rails.root.join("config/dropbox.yml"),
-                                        path: ":style/:id_:filename"
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
+        :storage => :dropbox,
+        :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+        :path => ":style/:id_:filename"
   end
 
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end

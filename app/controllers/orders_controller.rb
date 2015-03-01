13d185db5b40218a4779c1bfa49c7bafc48f32bb
Cody <wcodynorman@gmail.com>
2015-03-01 12:30:17 -0500

    added buy button, improved forms

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,58 @@
+class OrdersController < ApplicationController
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+
+  respond_to :html
+
+  def index
+    @orders = Order.all
+    respond_with(@orders)
+  end
+
+  def show
+    respond_with(@order)
+  end
+
+  def new
+    @order = Order.new
+    @listing = Listing.find(params[:listing_id])
+  end
+
+  def edit
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @listing = Listing.find(params[:listing_id])
+    @seller = @listing.user
+
+    @order.listing_id = @listing.id
+    @order.buyer_id = current_user.id
+    @order.seller_id = @seller.id
+
+    if @order.save
+      redirect_to root_url, notice: "Order successfully created!"
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @order.update(order_params)
+    respond_with(@order)
+  end
+
+  def destroy
+    @order.destroy
+    respond_with(@order)
+  end
+
+  private
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    def order_params
+      params.require(:order).permit(:address, :city, :state)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'listings#index'
   
-  resources :listings
+  resources :listings do
+    resources :orders
+  end
 
   get 'pages/about'
   get 'pages/contact'
